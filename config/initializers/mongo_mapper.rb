a23d01137f08e3c4b7b457d50963d23a804bb0a2
Jan Krutisch <jan@krutisch.de>
2010-05-06 17:06:39 +0200

    trying something to keep up the connection

--- config/initializers/mongo_mapper.rb
+++ config/initializers/mongo_mapper.rb
@@ -1,4 +1,5 @@
 File.open(File.join(RAILS_ROOT, 'config/mongodb.yml'), 'r') do |f|
   @settings = YAML.load(f)[RAILS_ENV]
 end
+MongoMapper.connection = Mongo::Connection.from_uri(@settings["connection"]) if @settings["connection"]
 MongoMapper.database = @settings["database"]
