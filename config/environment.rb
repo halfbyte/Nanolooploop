045ff106fd4e696dbf38d135cd40871cfa4afcdd
Jan Krutisch <jan@krutisch.de>
2010-05-07 16:33:23 +0200

    newrelic

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   config.gem 'joint'
   config.gem 'rpx_now'
   config.gem 'hoptoad_notifier'
+  config.gem "newrelic_rpm"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
